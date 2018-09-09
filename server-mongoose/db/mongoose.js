88a55ab85b84335b5f99e85a0c78e538672c6cbf
shaileh <shai.work1@gmail.com>
2018-09-09 23:50:51 +0300

    heroku config

--- server-mongoose/db/mongoose.js
+++ server-mongoose/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connection URL
-const url = process.env.MONGO_URL || 'mongodb://localhost:27017/TodoApp';
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
 
 mongoose.Promise = global.Promise;
 mongoose.connect(url);
