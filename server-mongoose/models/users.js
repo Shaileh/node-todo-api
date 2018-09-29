f4e60c4e0ad69336cfc9acbfbd1125678174ab22
Shaileh <shai.work1@gmail.com>
2018-09-30 01:52:08 +0300

    add authentication and tokens to users

--- server-mongoose/models/users.js
+++ server-mongoose/models/users.js
@@ -1,13 +1,66 @@
 // const mongoose = require('mongoose');
 const {mongoose} = require('./../db/mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash')
 
 
-var user = mongoose.model('Users',new mongoose.Schema({
+var UserSchema = new mongoose.Schema({
   email:{
       type: String,
       require: true,
       trim: true,
-      minlength:1
-    }}));
+      minlength:1,
+      unique: true,
+      // validate: (value) => { //use Promise function to validate email (async).
+      //   return new Promise((resolve , reject) => {
+      //        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      //       if(re.test(String(value).toLowerCase())){
+      //         resolve(true);
+      //       }else{
+      //         reject(false);
+      //       }
+      //   });
+      // }
+      validate: (value) => validator.isEmail(value)
+    },
+  password:{
+    type: String,
+    require: true,
+    minlength:6
+  },
+  tokens:[{
+    access:{
+      type: String,
+      require: true
+    },
+    token:{
+      type: String,
+      require: true
+    }
+  }]
+});
 
-    module.exports = {user};
+UserSchema.methods.generateAuthToken = function (){
+  var user = this;
+  var access = 'auth';
+  var token  = jwt.sign({_id:user._id.toHexString(), access },'abc123').toString();
+
+  user.tokens = user.tokens.concat({access,token});
+
+  return user.save().then(() => {
+    return token;
+  });
+
+};
+UserSchema.methods.toJSON = function(){
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject,['_id','email']);
+};
+
+
+var User = mongoose.model('Users',UserSchema);
+
+module.exports = {User};
--- server-mongoose/server.js
+++ server-mongoose/server.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 const {mongoose} = require('./db/mongoose');
 const {ObjectID} = require('mongodb');
 const {todo} = require('./models/todos');
-const {user} = require('./models/users');
+const {User} = require('./models/users');
 const bodyParser = require('body-parser');
 const express = require('express')
 const port = process.env.PORT || 3000;
@@ -14,13 +14,13 @@ var app = express()
 app.use(bodyParser.json());
 
 app.post('/todos',(req , res) => {
-  console.log(req.body);
+  // console.log(req.body);
   new todo (req.body).save().then((doc) => {
     res.send(doc);
-    console.log(`This doc: ${doc} is saved to db`);
+    // console.log(`This doc: ${doc} is saved to db`);
   },(e) => {
     res.status(400).send(e);
-    console.log(`error ${e}`);
+    // console.log(`error ${e}`);
   });
 });
 
@@ -93,6 +93,25 @@ app.patch('/todos/:id',(req,res) => {
     }
     res.status(200).send(updatedTodo);
   },(e) => {res.status(400).send(e)});
+});
+
+app.post('/users',(req,res) => {
+  var body = _.pick(req.body,['email','password']);
+  var user = new User(body);
+  // new user(req.body).save().then((doc) => {
+  //   res.status(200).send({doc});
+  // },(e) => {
+  //   res.status(400).send(e);
+  // });
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth',token).send({user});
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+
 });
 
 
